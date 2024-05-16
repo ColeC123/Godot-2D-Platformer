538c36077a79ca3feb7d26b99a26eca698c0ea6a
ColeC123 <colechapman924@gmail.com>
2024-05-16 13:04:29 -0700

    Add files via upload

---
+++ Labyrinth of the Wild/global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var gems_collected = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_debug("global ready!")
+	print_debug(gems_collected)
+	
+func _input(event):
+	if event.is_action_pressed("return_to_main_menu"):
+		get_tree().change_scene_to_file("res://main_menu.tscn")
+
